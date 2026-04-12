32e2c4cf1513f3d99492c84db07d500c7614fa38
topkech <alxez98@gmail.com>
2026-04-12 13:19:51 +0400

    fish prompt updates

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,26 +1,14 @@
 function fish_prompt
     set -l last_status $status
 
-    set -g __fish_git_prompt_showdirtystate 1
-    set -g __fish_git_prompt_showuntrackedfiles 1
-    set -g __fish_git_prompt_showupstream informative
-    set -g __fish_git_prompt_showcolorhints 1
-    set -g __fish_git_prompt_use_informative_chars 1
-
     set -l cwd (prompt_pwd)
-    set -l vcs (fish_vcs_prompt '(%s)' 2>/dev/null)
     set -l failure (__prompt_status_label "$last_status")
 
-    set_color normal
+    set_color yellow
     printf '⋊> '
 
     set_color $fish_color_cwd
     printf '%s' "$cwd"
-
-    if test -n "$vcs"
-        set_color brblack
-        printf ' %s' "$vcs"
-    end
 
     if test -n "$failure"
         set_color red
--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,7 +1,17 @@
 function fish_right_prompt
     set -g VIRTUAL_ENV_DISABLE_PROMPT true
+    set -g __fish_git_prompt_showdirtystate 1
+    set -g __fish_git_prompt_showuntrackedfiles 1
+    set -g __fish_git_prompt_showupstream informative
+    set -g __fish_git_prompt_showcolorhints 0
+    set -g __fish_git_prompt_use_informative_chars 1
 
     set -l parts
+    set -l vcs (fish_vcs_prompt '(%s)' 2>/dev/null)
+
+    if test -n "$vcs"
+        set parts $parts "$vcs"
+    end
 
     if set -q VIRTUAL_ENV
         set parts $parts (__prompt_venv_name)
